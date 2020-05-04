b4d769658ce4494b729f9cf95849cf8917095d44
Omkar Jadhav <omkarjadhav7110@gmail.com>
2020-05-04 20:10:20 +0530

    Async await uses and example

--- async-await.js
+++ async-await.js
@@ -45,7 +45,7 @@ async function calculateLength() {
     let length2 = await getLengthAfter1Seconds();  //Asynchronous - wait for result
     try {
         await getRejectedPromise();
-    } catch(e){
+    } catch (e) {
         console.log(e);
     }
     console.log('Array length is', length, length2);
@@ -55,3 +55,53 @@ calculateLength();  //synchronously called
 console.log('called synchronously.....', 2 + 555);  //synchronously called
 getLengthAfter2Seconds();  //synchronously called
 getLengthAfter1Seconds();  //synchronously called
+
+
+
+// +++++++++++++++++++++++++++++ EXAMPLE Using ++++++++++++
+// Callback Function
+// Async/await
+// Promise
+// try/catch
+
+var count = 0;
+var productArray = [];
+
+function findEvenOdd(n) {
+    return n % 2 === 0 ? 'Even' : 'Odd';
+}
+function checkForEvenOdd(num, callbackFun) {
+    return callbackFun(num);
+}
+function addProductInList(product) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            productArray.push(product);
+            let list = document.getElementById('productList');
+            list.innerHTML += `<li>${product.id} - ${product.product}</li>`;
+            resolve(productArray.length);
+        }, 1000);
+    })
+}
+
+async function addProduct() {
+    let btnEle = document.getElementById('addProduct');
+    btnEle.setAttribute('disabled', true);
+    let rawObj = {
+        id: ++count,
+        product: `Product-${count}`
+    }
+    try {
+        if (productArray.length < 5) {
+            let size = await addProductInList(rawObj);
+            let isSizeEvenOrOdd = checkForEvenOdd(size, findEvenOdd);
+            btnEle.disabled = false;
+            alert('Product added successfully');
+            console.log(size, isSizeEvenOrOdd);
+        } else {
+            throw new Error('Cannot add more than 5 products');
+        }
+    } catch (error) {
+        alert(error.message);
+    }
+}
